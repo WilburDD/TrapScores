41eca62831b277e074c92a0e2ba6c1c578386685
DD <doxiedavis@gmail.com>
2023-10-14 16:13:15 +0800

    Added finish round flow and general cosmetic clean-up.  Need app splash screen.

---
+++ TrapScores Watch App/FinishRound.swift
@@ -0,0 +1,18 @@
+//
+//  FinishRound.swift
+//  TrapScores Watch App
+//
+//  Created by Doxie Davis on 10/14/23.
+//
+
+import SwiftUI
+
+struct FinishRound: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinishRound()
+}

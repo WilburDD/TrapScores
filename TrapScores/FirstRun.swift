1f85b57ba558e6580aa742015861d2dd20cb40e5
DD <doxiedavis@gmail.com>
2023-10-17 20:55:22 +0800

    Hopefully v. 2.0

---
+++ TrapScores/FirstRun.swift
@@ -0,0 +1,18 @@
+//
+//  FirstRun.swift
+//  TrapScores
+//
+//  Created by Doxie Davis on 10/15/23.
+//
+
+import SwiftUI
+
+struct FirstRun: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FirstRun()
+}
